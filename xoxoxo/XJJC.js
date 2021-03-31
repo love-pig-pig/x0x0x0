1cbdea309990cb35fe060179bf740cfaf4ebf136
love-pig-pig <73279191+love-pig-pig@users.noreply.github.com>
2021-04-01 03:49:10 +0800

    Update XJJC.js

--- xoxoxo/XJJC.js
+++ xoxoxo/XJJC.js
@@ -951,7 +951,7 @@ async function tuanActivity() {
 }
 async function joinLeaderTuan() {
   $.tuanIdS = null;
-  if (!$.tuanIdS) await updateTuanIdsCDN('https://gitee.com/Soundantony/updateTeam/raw/master/shareCodes/jd_updateFactoryTuanId.json');
+  if (!$.tuanIdS) await updateTuanIdsCDN('https://github.com/love-pig-pig/Daily-tasks/raw/main/jd_updateFactoryTuanId.json');
   if ($.tuanIdS && $.tuanIdS.tuanIds) {
     for (let tuanId of $.tuanIdS.tuanIds) {
       if (!tuanId) continue
@@ -1259,7 +1259,7 @@ async function showMsg() {
 function readShareCode() {
   console.log(`开始`)
   return new Promise(async resolve => {
-    $.get({url: "https://gitee.com/Soundantony/RandomShareCode/raw/master/JD_Dream_Factory.json",headers:{
+    $.get({url: "https://github.com/love-pig-pig/Daily-tasks/raw/main/JD_Dream_Factory.json",headers:{
       "User-Agent": "Mozilla/5.0 (iPhone; CPU iPhone OS 13_2_3 like Mac OS X) AppleWebKit/605.1.15 (KHTML, like Gecko) Version/13.0.3 Mobile/15E148 Safari/604.1 Edg/87.0.4280.88"
     }}, async (err, resp, data) => {
       try {
